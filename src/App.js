c2d53988e7a1cbf932519f6051b93197e03cb8b1
lord-vector <r.anuj.agrawal@gmail.com>
2023-05-20 19:57:48 +0530

    login react

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="body">
+        <h1 className="head">
+          Linked<span>in</span>
+        </h1>
+        <div className="section">
+          <div className="content">
+            <div className="SectionHead">
+              <h1>Sign in</h1>
+              <p>Stay updated on your professional world</p>
+            </div>
+            <div className="form">
+              <input
+                type="text"
+                placeholder="Email or Phone"
+                className="inputs user"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="inputs pass"
+              />
+            </div>
+            <div className="forgetPass">Forgot Password?</div>
+            <button className="signin">Sign in</button>
+          </div>
+        </div>
+      </div>
+      <div className="foot">foot</div>
+    </>
+  );
+}
+
+export default App;
